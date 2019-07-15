8009c4a53bc982b2cd3a5a646da160aacb53fda6
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-15 14:29:24 +0200

    get references to ImageViews

--- app/src/main/java/yip/guesswhoihate/MainActivity.java
+++ app/src/main/java/yip/guesswhoihate/MainActivity.java
@@ -2,12 +2,35 @@ package yip.guesswhoihate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageView img_peter;
+    public ImageView img_laxfax;
+    public ImageView img_furb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img_peter = findViewById(R.id._img_peterlustig);
+        img_laxfax = findViewById(R.id._img_laxfax);
+        img_furb = findViewById(R.id._img_furb);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(),"resumed",Toast.LENGTH_LONG).show();
+
+
+
+
     }
 }
