3bb7026d36a2cb595189186062c87a17303c0ccf
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-19 13:24:39 +0200

    create test. db and copy create-statement

--- app/src/main/java/yip/guesswhoihate/db/DbHelper.java
+++ app/src/main/java/yip/guesswhoihate/db/DbHelper.java
@@ -3,13 +3,19 @@ package yip.guesswhoihate.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class DbHelper extends SQLiteOpenHelper {
 
 
     private static final String _DB_FILE_NAME = "hate_score.db";
     private static final int _DB_VERSION = 1;
-
+    private static final String _CREATE_TABLE = "CREATE TABLE \"hate_score\" (\n" +
+            "\t\"_id\"\tINTEGER,\n" +
+            "\t\"win\"\tINTEGER,\n" +
+            "\t\"fail\"\tINTEGER,\n" +
+            "\tPRIMARY KEY(\"_id\")\n" +
+            ")";
 
 
     public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -18,6 +24,9 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        //Toast.makeText(this, "Db created!", Toast.LENGTH_LONG).show();
+        db.execSQL(_CREATE_TABLE);
 
     }
 
