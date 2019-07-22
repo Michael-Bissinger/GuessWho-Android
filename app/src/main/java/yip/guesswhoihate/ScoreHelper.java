e4a202139afa228f430cacb4e3ca606671eedd7c
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-22 10:47:58 +0200

    implement method to get fail and win score

--- app/src/main/java/yip/guesswhoihate/ScoreHelper.java
+++ app/src/main/java/yip/guesswhoihate/ScoreHelper.java
@@ -1,4 +1,28 @@
 package yip.guesswhoihate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class ScoreHelper {
+
+
+
+    private static final String PREF_WIN = "pref_win";
+    private static final String PREF_FAIL = "pref_fail";
+
+    private static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(
+                PREF_WIN, Context.MODE_PRIVATE);
+    }
+
+    public static int getWinScore(Context context) {
+        return getPreferences(context).getInt(PREF_WIN, 0);
+
+    }
+
+    public static int getFailScore(Context context) {
+        return getPreferences(context).getInt(PREF_FAIL, 0);
+
+    }
+
 }
